2cc388b7b5f2da6acf9e4b86c5dedf490aaddbbe
Youssef Yasser <youssefyaser2020@gmail.com>
2025-12-19 00:27:15 +0200

    Solve 'Pick Problem' in file 00004-easy-pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,10 @@
-type MyPick<T, K> = any
+type MyPick<T extends Record<PropertyKey, any>, K extends keyof T> = { [P in K]: T[P] }
+
+// here i used 'extends Record<PropertyKey, any>' to handle more test cases
+
+type X = MyPick<Todo, 'title' | 'completed'>
+interface Todo {
+  title: number
+  description: string
+  completed: boolean
+}
